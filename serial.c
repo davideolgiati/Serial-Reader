0fb36cf1897694dff071259a8a6021bef32b2a5a
Olgiati Davide <davide.olgiati@hotmail.it>
2014-12-03 10:52:20 +0100

    serial.c
    main source. it need serial.h

---
+++ serial.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "killerutils.h"
+ 
+int main(int argc, char* argv[])
+{
+char input;
+bool question = 1;
+int com,i=0,ch;			
+struct termios tattr;		
+const char *options = "ho";
+ 
+ 
+  system("clear");
+  ch = (getopt(argc, argv, options));
+  switch (ch)
+  {
+   case 'h':
+     help();
+     return 0;
+     break;
+   case 'o':
+     printf("\n\t\t\t\t\033[1;31mONE-SHOT MODE\033[0m\n\n");
+     com=init(com);
+     tattr=silent(tattr, com);
+     sread(com, input, i);
+     return 0;
+     break;
+  }
+  com=init(com);
+  tattr=full(tattr, com);
+  while(question)
+  {
+   i=sread(com, input, i);
+  }
+  return 0;				
+}
