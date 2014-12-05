efc1c63016335a03e2db0f4d5faf15a9490c75df
Olgiati Davide <davide.olgiati@hotmail.it>
2014-12-05 09:13:06 +0100

    Update serial.h

--- serial.h
+++ serial.h
@@ -1,32 +1,47 @@
 struct termios full (struct termios tattr ,int com)
 {
   printf("\t Acquisisco informazioni sulla Seriale ....");
+  
   tcgetattr (com, &tattr);
   tattr.c_cflag &= ~CSIZE;
+  
   printf("\t\033[1;32m Acquisite! \033[0m\n");
   printf("\t Disabilito i Flag di Lettura non necessari ....");
+  
   tattr.c_iflag &= ~(INLCR|IGNCR|ICRNL|IXON);
   tattr.c_oflag &= ~OPOST;
+  
   printf("\t\033[1;32m Disabilitati! \033[0m\n");
   printf("\t Abilito la lettura a 7bit ....");
+  
   tattr.c_cflag = CS7 | CREAD | CLOCAL; 
+  
   printf("\t\t\t\033[1;32m Abilitata! \033[0m\n");
   printf("\t Prevengo output errati ....");
+  
   tattr.c_lflag &= ~(ICANON|ECHO);
+  
   printf("\t\t\t\033[1;32m Prevenuti! \033[0m\n");
   printf("\t Imposto un solo stop bit ....");
+  
   tattr.c_cflag &= ~CSTOPB;
+  
   printf("\t\t\t\033[1;32m Impostato! \033[0m\n");
   printf("\t Setup timeout seriale e caratteri ....");	
+  
   tattr.c_cc[VMIN] = 1;			
   tattr.c_cc[VTIME] = 0;
+  
   printf("\t\t\033[1;32m Setup avvenuto! \033[0m\n");
   printf("\t Setto la velocità di lettura! ....");		
+  
   cfsetispeed (&tattr, B9600);		
+  
   printf("\t\t\033[1;32m Setup avvenuto! \033[0m\n");
- 
   printf("\n\t Applico i Cambiamenti ....");
+ 
   tcsetattr (com, TCSANOW, &tattr);	
+ 
   printf("\t\t\t\033[1;32m Applicati! \033[0m\n\n\n");
   return tattr;
 }
